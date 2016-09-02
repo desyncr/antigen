e9481534ff46c202bb5eb907bd27c681e22d04c2
DC* <syncr@riseup.net>
2016-09-02 17:20:58 -0300

    [WIP] Refactor bundle cache

--- ext/zcache.zsh
+++ ext/zcache.zsh
@@ -1,25 +1,31 @@
-local _ZCACHE_PAYLOAD_PATH="$_ANTIGEN_INSTALL_DIR/.cache/.zcache-payload"
-local _ZCACHE_META_PATH="$_ANTIGEN_INSTALL_DIR/.cache/.zcache-meta"
-local _ZCACHE_CACHE_LOADED=false
+export _ZCACHE_PAYLOAD_PATH="$_ANTIGEN_INSTALL_DIR/.cache/.zcache-payload"
+export _ZCACHE_META_PATH="$_ANTIGEN_INSTALL_DIR/.cache/.zcache-meta"
 local -a _ZCACHE_BUNDLES
 
 -zcache-generate-cache () {
   for bundle in $_ZCACHE_BUNDLES; do
-    echo "-zcache-antigen-bundle $bundle"
+    -zcache-antigen-bundle $bundle
   done
 
   local _zcache_extensions_paths=''
-  _ZCACHE_PAYLOAD_PATH="$_ANTIGEN_INSTALL_DIR/.cache/.zcache-payload"
+  local _zcache_bundles_meta=''
 
   echo "#-- START ZCACHE GENERATED FILE" >>! $_ZCACHE_PAYLOAD_PATH;
   for bundle in $_ZCACHE_BUNDLES; do
       # -antigen-load-list "$url" "$loc" "$make_local_clone"
       eval "$(-antigen-parse-bundle ${=bundle})"
+      _zcache_bundles_meta="$url $loc $branch $make_local_clone $btype\n$_zcache_bundles_meta"
       # url=$(-antigen-get-clone-dir "$url")
       -antigen-load-list "$url" "$loc" "$make_local_clone" | while read line; do
         echo "#-- SOURCE: $line" >>! $_ZCACHE_PAYLOAD_PATH
         if [[ -f "$line" ]]; then
-            cat $line >>! $_ZCACHE_PAYLOAD_PATH
+            # TODO Create -zcache-per-parse-source function in order to be able to override it
+            cat $line \
+                | sed $'/\${0/i\\\n__ZCACHE_FILE_PATH=\''$line$'\'\n' \
+                | sed -e "s/\${0/\${__ZCACHE_FILE_PATH/" \
+                | sed $'/\$0/i\\\n__ZCACHE_FILE_PATH=\''$line$'\'\n' \
+                | sed -e "s/\$0/\$__ZCACHE_FILE_PATH/" \
+                >>! $_ZCACHE_PAYLOAD_PATH
         elif [[ -d "$line" ]]; then
             _zcache_extensions_paths="$line\n$_zcache_extensions_paths"
         fi
@@ -28,21 +34,26 @@ local -a _ZCACHE_BUNDLES
   done
   echo "fpath=($_zcache_extensions_paths $fpath);" >>! $_ZCACHE_PAYLOAD_PATH
   echo "export _ANTIGEN_BUNDLE_RECORD=\"${(j:\n:)_ZCACHE_BUNDLES}\"" >>! $_ZCACHE_PAYLOAD_PATH
+  echo "export _ZCACHE_CACHE_LOADED=true" >>! $_ZCACHE_PAYLOAD_PATH
   echo "#-- END ZCACHE GENERATED FILE" >>! $_ZCACHE_PAYLOAD_PATH;
+
+  echo "$_zcache_bundles_meta" >>! $_ZCACHE_META_PATH
+}
+
+-zcache-cache-exists () {
+  [[ -f "$_ZCACHE_PAYLOAD_PATH" ]] && return true
 }
 
 -zcache-antigen-hook () {
-  if [[ "$1" == "apply" ]]; then
-    -zcache-unhook-antigen
-  fi
-
   if [[ "$1" == "theme" ]]; then
     antigen-theme "$2" "$3" "$4"
     return
   fi
 
   if [[ "$1" == "apply" ]]; then
-    -zcache-generate-cache
+    -zcache-unhook-antigen
+    ! -zcache-cache-exists && -zcache-generate-cache
+    [[ ! $_ZCACHE_CACHE_LOADED ]] && source "$_ZCACHE_PAYLOAD_PATH"
   else
     _ZCACHE_BUNDLES+=("$*")
   fi
@@ -69,15 +80,10 @@ local -a _ZCACHE_BUNDLES
 
 -zcache-start () {
   -zcache-hook-antigen
-  if [ -f "$_ZCACHE_PAYLOAD_PATH" ] ; then
-    source "$_ZCACHE_PAYLOAD_PATH" # cache exists, load it
-    _ANTIGEN_BUNDLE_RECORD=$(cat $_ZCACHE_META_PATH)
-    _ZCACHE_CACHE_LOADED=true
-  fi
 }
 
 antigen-cache-reset () {
-  [ -f "$_ZCACHE_META_PATH" ] && rm "$_ZCACHE_META_PATH"
-  [ -f "$_ZCACHE_PAYLOAD_PATH" ] && rm "$_ZCACHE_PAYLOAD_PATH"
+  [[ -f "$_ZCACHE_META_PATH" ]] && rm "$_ZCACHE_META_PATH"
+  [[ -f "$_ZCACHE_PAYLOAD_PATH" ]] && rm "$_ZCACHE_PAYLOAD_PATH"
   echo 'Done. Please open a new shell to see the changes.'
 }
